2f0ca2df80236707911bde20bd3edd7a08140a80
Maria7867 <49097721+Maria7867@users.noreply.github.com>
2022-03-10 19:55:50 +0100

    Drive nivel 2 acabado

--- client.py
+++ client.py
@@ -12,9 +12,60 @@ from google.oauth2.credentials import Credentials
 from zipfile import ZipFile
 import os #para conseguir el final del file_path
 
+# import required module
+from cryptography.fernet import Fernet
+
+
 # If modifying these scopes, delete the file token.json.
 #SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def level1(file_path):
+    print("zip name: ")
+    zip_name = input()
+    zip_name = zip_name + '.zip'
+    myzip=ZipFile(zip_name, 'w')
+    myzip.write(os.path.basename(file_path)) #no se si os.path.basename funciona para windows
+    myzip.close()
+    return zip_name
+
+def level2 (drive_api, file_name, file_path):
+    #We have to make a request hash to tell the google API what we're giving it
+    body = {'name': file_name, 'mimeType': 'application/zip'} #application/vnd.google-apps.folder
+
+    zip_name=level1(file_path)
+    # key generation
+    key = Fernet.generate_key()
+
+    # string the key in a file
+    with open('filekey.key', 'wb') as filekey:
+        filekey.write(key)
+    # opening the key
+    with open('filekey.key', 'rb') as filekey:
+    	key = filekey.read()
+
+    # using the generated key
+    fernet = Fernet(key)
+
+    # opening the original file to encrypt
+    with open(zip_name, 'rb') as file:
+    	original = file.read()
+
+    # encrypting the file
+    encrypted = fernet.encrypt(original)
+
+    # opening the file in write mode and
+    # writing the encrypted data
+    with open(zip_name, 'wb') as encrypted_file:
+    	encrypted_file.write(encrypted)
+    print("encrypted", encrypted_file)
+
+
+    media = MediaFileUpload(zip_name, mimetype='application/zip')#'/home/maria/Documentos/TFG/drivepy/avantasia_cover.jpeg'
+    #Now we're doing the actual post, creating a new file of the uploaded type
+    fiahl = drive_api.files().create(body=body, media_body=media).execute()
+    #Because verbosity is nice
+
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
@@ -69,23 +120,14 @@ def main():
         #We have to make a request hash to tell the google API what we're giving it
         body = {'name': file_name, 'mimeType': 'application/zip'} #application/vnd.google-apps.folder
 
-        print("zip name: ")
-        zip_name = input()
-        zip_name = zip_name + '.zip'
-        myzip=ZipFile(zip_name, 'w')
-        myzip.write(os.path.basename(file_path)) #no se si os.path.basename funciona para windows
-        myzip.close()
-        print ("1...........")
-
+        zip_name=level1(file_path)
         media = MediaFileUpload(zip_name, mimetype='application/zip')#'/home/maria/Documentos/TFG/drivepy/avantasia_cover.jpeg'
-        print ("2...........")
-
     	#Now we're doing the actual post, creating a new file of the uploaded type
         fiahl = drive_api.files().create(body=body, media_body=media).execute()
-        print ("3...........")
-
     	#Because verbosity is nice
         print ("Created file '%s' id '%s'." % (fiahl.get('name'), fiahl.get('id')))
+    if level=='2':
+        level2(drive_api, file_name, file_path)
 
 if __name__ == '__main__':
     main()
