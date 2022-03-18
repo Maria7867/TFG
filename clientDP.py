c0e7c9f91c8e3e253fd366298492b8079f5ee883
Maria7867 <49097721+Maria7867@users.noreply.github.com>
2022-03-18 01:25:38 +0100

    Dropbox async

--- clientDP.py
+++ clientDP.py
@@ -2,22 +2,52 @@
 import dropbox
 from dropbox import DropboxOAuth2FlowNoRedirect
 from zipfile import ZipFile
-import os #para conseguir el final del file_path
-
+import os #para conseguir el final del file_path y para conseguir el tamaño de file
+import asyncio
 # import required module
 from cryptography.fernet import Fernet
+from filesplit.split import Split
 
-def level1(file_from):
+#D:\Code\p.txt
+def crear():
+    path = os.getcwd()
+    crear_path=path+"\dividir"
+    try:
+        os.mkdir(crear_path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+    return crear_path
+
+async def dividir_enviar(file_from, dbx):
+    file_size = os.path.getsize(file_from)
+    print("size: ", file_size)
+    path = crear()
+    split = Split(file_from, path)
+    size_divide=int(file_size/4)
+    print("size: ", size_divide)
+    split_size = split.bysize (size_divide)
+    files = os.listdir(path)
+    for f in files:
+        file_to="/"+f
+        file_de=path+"\\"+f
+        if f != "manifest":
+            dbx.files_upload(open(file_de, 'rb').read(), file_to)
+            print(f)
+            await asyncio.sleep(1)
+
+def zip_file(file_from):
     print("zip name: ")
     zip_name = input()
     zip_name = zip_name + '.zip'
     myzip=ZipFile(zip_name, 'w')
-    myzip.write(os.path.basename(file_from)) #no se si os.path.basename funciona para windows
+    myzip.write(file_from) #no se si os.path.basename funciona para windows
     myzip.close()
     return zip_name
 
-def level2 (dbx, file_from, file_to):
-    zip_name=level1(file_from)
+def cifrar (dbx, file_from):
+    zip_name=zip_file(file_from)
     # key generation
     key = Fernet.generate_key()
 
@@ -44,7 +74,7 @@ def level2 (dbx, file_from, file_to):
     	encrypted_file.write(encrypted)
     print("encrypted", encrypted_file)
 
-    dbx.files_upload(open(zip_name, 'rb').read(), file_to)
+    return zip_name
 
 
 def main():
@@ -92,21 +122,32 @@ def main():
     '''
     print("choose level: ")
     level = input()
-    print ("File path from your pc: ") #/home/maria/Documentos/TFG/Dropbox/avantasia_cover.jpeg
+    print ("File path from your pc: ") #/home/maria/Documentos/TFG/Dropbox/avantasia_cover.jpeg || D:\Code\p.txt
     file_from = input()
     print ("File path from dropbox: ") #/prueba1/avantasia_cover.jpeg // /avantasia_cover.jpeg o /avantasia_cover.zip
     file_to = input ()
 
-    dbx = dropbox.Dropbox('sl.BCcE9HL-ahZz-UaduAokFItU-zeMDk_mP6jL-hMLpCDDYV_rkQsSLUulhOzWaAtbhycM-YrSLisnFH-L54UmJ4DVa321tTgTv5YWIJWznVIxBQsuxMqL_i-cOM91QKI7PKKchhk') #Este es el token, si no funciona es porque se habrá caducado y hay que generar otro.
+    dbx = dropbox.Dropbox('sl.BD-tvmEBYhZYzko38qJXe7Hd5HdHUvUgSE3Sbt-zlaCJLBNnLd47FLOlijOJqU3EXrQnerUHaM5bII91VLZKWP4kfmDLKTLfJsne3AVf_PeEb1o4k5-ZgV5OmrJkklgUhqRGHHc') #Este es el token, si no funciona es porque se habrá caducado y hay que generar otro.
     #Genera otro. Y si pasa en drive borra el archivo y ejecuta el código otra vez
-
-    if level=='0':
-        dbx.files_upload(open(file_from, 'rb').read(), file_to)
-    if level=='1':
-        zip_name=level1(file_from)
-        dbx.files_upload(open(zip_name, 'rb').read(), file_to)
-    if level=='2':
-        level2(dbx, file_from, file_to)
+    file_size = os.path.getsize(file_from)
+    if file_size>200:
+        if level=='0':
+            asyncio.run(dividir_enviar(file_from, dbx))
+        if level=='1':
+            zip_name=zip_file(file_from)
+            asyncio.run(dividir_enviar(zip_name, dbx))
+        if level=='2':
+            zip_name=cifrar(dbx, file_from)
+            asyncio.run(dividir_enviar(zip_name, dbx))
+    else:
+        if level=='0':
+            dbx.files_upload(open(file_from, 'rb').read(), file_to)
+        if level=='1':
+            zip_name=zip_file(file_from)
+            dbx.files_upload(open(zip_name, 'rb').read(), file_to)
+        if level=='2':
+            zip_name=cifrar(dbx, file_from)
+            dbx.files_upload(open(zip_name, 'rb').read(), file_to)
 
 if __name__ == '__main__':
     main()
