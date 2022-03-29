e417347bc3859868d8f51a3389bc2b9ba00588ab
Maria7867 <49097721+Maria7867@users.noreply.github.com>
2022-03-29 20:08:10 +0200

    merge y decrypt codes

---
+++ decrypt.py
@@ -0,0 +1,30 @@
+from cryptography.fernet import Fernet
+
+def decrypt(filename, key_path):
+    """
+    Given a filename (str) and key (bytes), it decrypts the file and write it
+    """
+    with open(filename, "rb") as file:
+        # read the encrypted data
+        encrypted_data = file.read()
+    # opening the key
+    with open(key_path, 'rb') as filekey:
+        key = filekey.read()
+    f = Fernet(key)
+    # decrypt data
+    decrypted_data = f.decrypt(encrypted_data)
+    # write the original file
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+    print("encrypted", file)
+
+def main():
+
+    print("key path: ")
+    path=input() #D:\Code\decrypt\filekey.key
+    print("path of the file you want to decrypt: ")
+    filename=input() #D:\Code\decrypt\filekey.key
+    decrypt(filename,path)
+
+if __name__ == '__main__':
+    main()
