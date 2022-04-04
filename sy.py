7f88e04db541182b8cce62f59d6a53fe41cb8d08
Maria7867 <49097721+Maria7867@users.noreply.github.com>
2022-04-04 19:04:09 +0200

    input con config

--- sy.py
+++ sy.py
@@ -1,31 +1,25 @@
-#import dbx
-#from prueba import pr
-'''
-from dbx import clientDP
-from drive import client
-#from mega import clientMU
-from cliente import cl
-from servidor import sv
-from merge import merge
-from decrypt import decrypt
-'''
 from code.dbx import clientDP
 from code.drive import client
-#from code.mega import clientMU
+from code.mega import clientMU
 from code.cliente import cl
 from code.servidor import sv
 from code.merge import merge
 from code.decrypt import decrypt
-
+import configparser
+'''
 print("module: ")
 module=input()
+'''
+config = configparser.ConfigParser()
+config.read('config.ini')
+module=config['MODULO']['MOD']
 
 if module=='db':
     clientDP.dbx_main()
 if module=='dv':
     client.dv_main()
-#if module=='mu':
-#    clientMU.mu_main()
+if module=='mu':
+    clientMU.mu_main()
 if module=='cl':
     cl.cl_main()
 if module=='sv':
