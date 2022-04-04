1fe3317b469a5b08ff934c5ae33fd39b2617c65a
Maria7867 <49097721+Maria7867@users.noreply.github.com>
2022-04-04 19:02:48 +0200

    input con config

--- code/merge/merge.py
+++ code/merge/merge.py
@@ -12,10 +12,16 @@ def mg_main():
         path_partes=path_actual+"/partes"
     '''
     path_actual= os.getcwd()
+    '''
     print("path of the folder with the file parts: ")
     path_partes=input()
     print("name of the merged file: ")
     name_file=input()
+    '''
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    path_partes = config['MERGE']['PATH_PARTES']
+    name_file = config['MERGE']['NAME_FILE']
     juntar = Merge(path_partes, path_actual, name_file)
     juntar.merge()
 
